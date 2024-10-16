6f7ab54616e7180721464e11368e98eacd838dbc
PythonPro78 <91122678@mvusd.net>
2024-10-15 21:40:44 -0700

    Added full entity constructor

--- src/Entity.java
+++ src/Entity.java
@@ -1,4 +1,17 @@
 public class Entity
 {
+    private static int numEntities;
 
+    private int health;
+    private int money;
+    private boolean alive;
+
+    public Entity(int health, int money, boolean alive)
+    {
+        numEntities ++;
+
+        this.health = health;
+        this.money = money;
+        this.alive = alive;
+    }
 }
