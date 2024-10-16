d0b778a943a6815e6a86bca68201e6663433a556
PythonPro78 <91122678@mvusd.net>
2024-10-16 06:58:31 -0700

    Tested the thing

--- src/Main.java
+++ src/Main.java
@@ -4,6 +4,30 @@ public class Main
 {
     public static void main(String[] args)
     {
-        System.out.println("Hello world!");
+        Staff staffOfTheKingsCryptOfTheForsaken = new Staff(5, 15, "Nicke");
+        Staff magicStick = new Staff(0, 7);
+
+        Wizard gandalf = new Wizard(50, 8, 40, staffOfTheKingsCryptOfTheForsaken);
+        Wizard dumboldore = new Wizard(40, 49, true, 70, magicStick);
+
+        System.out.println(gandalf);
+        System.out.println(dumboldore);
+        System.out.println();
+
+        gandalf.boostedAttack(dumboldore, 12, 12);
+        dumboldore.boostedAttack(gandalf, 9, 30);
+        System.out.println("Attack");
+        System.out.println();
+
+        System.out.println(gandalf);
+        System.out.println(dumboldore);
+        System.out.println();
+
+        gandalf.boostedAttack(dumboldore, 8, 10);
+        System.out.println("Attack");
+        System.out.println();
+
+        System.out.println(gandalf);   
+        System.out.println(dumboldore);
     }
 }
