d9d017f4dda7e2d530831367626d1e1ef78b7fd5
PythonPro78 <91122678@mvusd.net>
2024-10-15 21:58:18 -0700

    Created Wizard class

--- src/Entity.java
+++ src/Entity.java
@@ -106,7 +106,6 @@ public class Entity
     }
 
     // BRAIN METHODS
-
     @Override
     public String toString()
     {
---
+++ src/Wizard.java
@@ -0,0 +1,18 @@
+public class Wizard extends Entity
+{
+    // VARIABLES
+    public int mana;
+//    public Staff staff;
+
+    // CONSTRUCTORS
+//    public void Wizard(int health, int money, boolean alive, int mana)
+//    {
+//
+//    }
+
+    // SETTERS
+
+    // GETTERS
+
+    // BRAIN METHODS
+}
